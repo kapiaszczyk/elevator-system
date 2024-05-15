88dff50747cc38b1b0c36cfd00b7640ea735f2ee
kapiaszczyk <41442206+kapiaszczyk@users.noreply.github.com>
2024-05-15 11:50:52 +0200

    Change elevator speed constant

--- src/main/java/org/example/constants/Constants.java
+++ src/main/java/org/example/constants/Constants.java
@@ -2,7 +2,7 @@ package org.example.constants;
 
 public class Constants {
 
-    public static final int ELEVATOR_SPEED = 2;
+    public static final int ELEVATOR_SPEED = 12;
     public static final int NUM_FLOORS = 20;
     public static final int NUM_ELEVATORS = 16;
 
